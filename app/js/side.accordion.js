015bbd33ea9b8b16940a810beee2313f160b8132
Anton Anton <Toni95Kova@mail.com>
2023-07-29 17:58:35 +0300

    commit27

--- app/js/side.accordion.js
+++ app/js/side.accordion.js
@@ -1,2 +1,32 @@
 const allButton = document.querySelector('.catalog__all-button')
+const buttonOnePage = document.querySelectorAll('.catalog__side-accordion')
+
+buttonOnePage.forEach(item => {
+  item.addEventListener('click', openSideAccordion)
+})
+
+function openSideAccordion (event) {
+  const nearestParent = event.target.closest('.catalog__side-accordion') 
+  const currentContent = event.target.nextElementSibling
+  nearestParent.classList.toggle('open-side-accordion')
+  nearestParent.classList.toggle('background-active')
+
+  if (nearestParent.classList.contains('open-side-accordion')) {
+    currentContent.style.maxHeight = currentContent.scrollHeight + 'px'
+  } else {
+    currentContent.style.maxHeight = 0 
+  }
+}
+
+document.addEventListener('click', (event) => {
+  const target = event.target
+  
+  if (target.classList.contains('catalog__all-button')) {
+    allButton.classList.add('background-active')
+  } else {
+    // allButton.classList.remove('background-active')
+  }
+})
+
+
 
