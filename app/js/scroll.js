9f1323499eb196c2d2644192b0a6c17d57c44441
Anton Anton <Toni95Kova@mail.com>
2023-07-30 17:46:05 +0300

    commit32

--- app/js/scroll.js
+++ app/js/scroll.js
@@ -2,36 +2,21 @@
 const menulinks = document.querySelectorAll('.header__link[data-goto')
 menulinks.forEach(item => {
   item.addEventListener('click', scrollToSection)
-
-  function scrollToSection (e) {
-    const target = e.target
-    const gotoBlock = document.querySelector(target.dataset.goto)
-    const blockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header').offsetHeight
-    window.scrollTo({
-      top:blockValue,
-      behavior:'smooth'
-    })
-    e.preventDefault()
-  }
 });
+function scrollToSection (e) {
+  const target = e.target
+  const gotoBlock = document.querySelector(target.dataset.goto)
+  const blockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header').offsetHeight
+  window.scrollTo({
+    top:blockValue,
+    behavior:'smooth'
+  })
+  e.preventDefault()
+}
 
-// const anhors = document.querySelectorAll('a[href*="#"')
+const burgerlink = document.querySelectorAll('.header__burger-link')
 
-// for (let elem of anhors) {
-//   elem.addEventListener('click', (e) => {
-//     e.preventDefault()
-//     const blockId = elem.getAttribute('href')
-//     document.querySelector('' + blockId).scrollIntoView({
-//       behavior:'smooth',
-//       block:'start'
-//     })
-//      const  header = document.querySelector('.header').offsetHeight
-//      console.log(header)
+// burgerlink.forEach(item => {
+//   item
+// })
 
-    
-//   })
-// }
-
-// const blockSection = document.querySelector('.news') 
-    // const blockValue = blockSection.getBoundingClientRect().top + pageYOffset - document.querySelector('.header').offsetHeight
-    // console.log(blockValue)
