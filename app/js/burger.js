3dbf4467bd9ef7e1a007476a3c0b09515b70d8b1
Anton Anton <Toni95Kova@mail.com>
2023-07-31 16:42:42 +0300

    commit33

--- app/js/burger.js
+++ app/js/burger.js
@@ -2,8 +2,23 @@ const openBtn = document.querySelector('.header__burger-open')
 openBtn.addEventListener('click', openBurger)
 
 function openBurger () {
+  const burgerlink = document.querySelectorAll('.header__burger-link')
   const burgerWrapper = document.querySelector('.header__burger-wrapper')
   burgerWrapper.classList.toggle('burger-open')
   openBtn.classList.toggle('active-icon-burger')
   document.body.classList.toggle('no-scroll')
+  // debugger
+  burgerlink.forEach(item => {
+    item.addEventListener('click', closeBurger)
+  })
+  
 }
+function closeBurger () {
+  // console.log(burgerWrapper)
+  const burgerWrapper = document.querySelector('.header__burger-wrapper')
+  burgerWrapper.classList.toggle('burger-open')
+  openBtn.classList.toggle('active-icon-burger')
+  document.body.classList.toggle('no-scroll')
+  // popup.classList.toggle('burger-open')
+}
+// console.log(burgerlink)
--- app/js/scroll.js
+++ app/js/scroll.js
@@ -1,8 +1,12 @@
 
-const menulinks = document.querySelectorAll('.header__link[data-goto')
+const menulinks = document.querySelectorAll('.header__link[data-goto]')
+const birgerLunk = document.querySelectorAll('.header__burger-link')
 menulinks.forEach(item => {
   item.addEventListener('click', scrollToSection)
 });
+birgerLunk.forEach(item => {
+  item.addEventListener('click',scrollToSection)
+})
 function scrollToSection (e) {
   const target = e.target
   const gotoBlock = document.querySelector(target.dataset.goto)
@@ -14,9 +18,6 @@ function scrollToSection (e) {
   e.preventDefault()
 }
 
-const burgerlink = document.querySelectorAll('.header__burger-link')
 
-// burgerlink.forEach(item => {
-//   item
-// })
+
 
