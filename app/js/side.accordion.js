37e9b5f74c3b779fbf68e23376a8c67640443f75
Anton Anton <Toni95Kova@mail.com>
2023-08-05 14:02:15 +0300

    commit34

--- app/js/side.accordion.js
+++ app/js/side.accordion.js
@@ -6,27 +6,36 @@ buttonOnePage.forEach(item => {
 })
 
 function openSideAccordion (event) {
+  const allButton = document.querySelector('.catalog__all-button')
   const nearestParent = event.target.closest('.catalog__side-accordion') 
   const currentContent = event.target.nextElementSibling
   nearestParent.classList.toggle('open-side-accordion')
   nearestParent.classList.toggle('background-active')
+  allButton.classList.remove('background-active')
 
   if (nearestParent.classList.contains('open-side-accordion')) {
     currentContent.style.maxHeight = currentContent.scrollHeight + 'px'
   } else {
     currentContent.style.maxHeight = 0 
   }
+  document.addEventListener('click', (event) => {
+    const target = event.target
+    
+    if (target.classList.contains('catalog__all-button')) {
+      allButton.classList.add('background-active')
+      currentContent.style.maxHeight = 0 
+      if (nearestParent.classList.contains('open-side-accordion')){
+        nearestParent.classList.remove('open-side-accordion')
+      } else {
+
+      }
+      buttonOnePage.forEach(item => {
+        item.classList.remove('background-active')
+      })
+    } 
+  })
 }
 
-document.addEventListener('click', (event) => {
-  const target = event.target
-  
-  if (target.classList.contains('catalog__all-button')) {
-    allButton.classList.add('background-active')
-  } else {
-    // allButton.classList.remove('background-active')
-  }
-})
 
 
 
